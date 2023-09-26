922acc7bef7d518c974f81c9e46718a733fc80a6
Tyuiu.FedorenkoKS@outlook.com <kirilll.fedorenko.2020@mail.ru>
2023-09-26 20:06:01 +0300

    Task2

---
+++ Tyuiu.FedorenkoKS.Sprint0.Task2.V0.Test/DataServiseTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.FedorenkoKS.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.FedorenkoKS.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiseTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            //область создания методов тестирования, методов из библиотеки
+            var name = "Кирилл";
+            var res = DataService.GetMessage(name);
+
+            //вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет, Кирилл", res);
+        }
+    }
+}
---
+++ Tyuiu.FedorenkoKS.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.FedorenkoKS.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.FedorenkoKS.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a63ce59d-fb38-49b2-8245-d75e52298bc2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
