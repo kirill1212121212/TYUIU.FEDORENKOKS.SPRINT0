5cacef00d0d45ce42728efb95cdfa7879ffc1be4
Tyuiu.FedorenkoKS@outlook.com <kirilll.fedorenko.2020@mail.ru>
2023-09-26 11:46:59 +0300

    Task1

---
+++ Tyuiu.FedorenkoKS.Sprint0Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.FedorenkoKS.Sprint0Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Kirill";
+            int age = 18;
+            Console.WriteLine("Zdravstvuyte, menya zovut " + name + " mne " + age + " Let! ");
+            Console.ReadKey();
+        }
+    }
+}
